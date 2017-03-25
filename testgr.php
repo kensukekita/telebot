92e63036271c60c26fd5e3afd80c521c0536c422
kensukekita <rbert379@gmail.com>
2017-03-25 19:24:44 -0300

    updated to test
    test

--- testgr.php
+++ testgr.php
@@ -3,10 +3,8 @@ require 'vendor/autoload.php';
 use Telegram\Bot\Api;
 
 $telegram = new Api('376072170:AAE2_y5EpVRCwc8Xbe-GHJLNP9yUdP2Zzto');
-
 $response = $telegram->sendAudio([
-  'chat_id' => '237836796', 
-  'chat_id' => '-1001109283383', 
+  'chat_id' => '237836796, -1001109283383', 
   'audio' => 's.mp3',
 ]);
 
