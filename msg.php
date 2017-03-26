81c557ac45cf008c6967c6865f2bff4509b2445c
kensukekita <rbert379@gmail.com>
2017-03-26 11:13:40 -0300

    Update msg.php

--- msg.php
+++ msg.php
@@ -13,4 +13,11 @@ $response = $telegram->sendMessage([
 ]);
 
 
+
 $messageId = $response->getMessageId();
+?>
+<form method="POST" action="msg.php">
+<input name="id" id="id" type="text" value="<? echo $chatid ?>" />
+<input name="msg" id="msg" type="text" plceholder="Mensaje" />
+<input type="submit" value="enviar">
+</form>
